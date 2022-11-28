de829d42607ac95445af297104308afb86ecb1ff
Evans Wanguba <ewanguba@gmail.com>
2022-11-28 09:32:59 +0300

    my changes

--- app/Models/CustomerLogin.php
+++ app/Models/CustomerLogin.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class CustomerLogin extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'phone', 'password',
+    ];
 }
