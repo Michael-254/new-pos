ed4aba756b900b1fd8a6acedc2e4152f875903fa
Evans Wanguba <ewanguba@gmail.com>
2022-11-30 16:49:46 +0300

    Customer dashboard

--- app/Http/Controllers/Admin/BusinessSettingsController.php
+++ app/Http/Controllers/Admin/BusinessSettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\BusinessSetting;
+use App\Models\MpesaCredential;
 use Illuminate\Support\Facades\DB;
 use App\CPU\Helpers;
 use Brian2694\Toastr\Facades\Toastr;
@@ -18,58 +19,40 @@ class BusinessSettingsController extends Controller
     }
     public function shop_setup(Request $request)
     {
+        $company_id = auth()->guard('admin')->user()->company_id;
+        $curr_logo = BusinessSetting::where(['company_id' => $company_id])->first();
+
         if ($request->pagination_limit == 0) {
             Toastr::warning(translate('pagination_limit_is_required'));
             return back();
         }
 
-        DB::table('business_settings')->updateOrInsert(['key' => 'shop_name'], [
-            'value' => $request['shop_name']
+        DB::table('business_settings')->updateOrInsert(['company_id' => $company_id], [
+            'shop_name' => $request['shop_name'],
+            'shop_email' => $request['shop_email'],
+            'shop_phone' => $request['shop_phone'],
+            'shop_address' => $request['shop_address'],
+            'pagination_limit' => $request['pagination_limit'],
+            'stock_limit' => $request['stock_limit'],
+            'currency' => $request['currency'],
+            'country' => $request['country'],
+            'footer_text' => $request['footer_text'],
+            'shop_logo' => $request->has('shop_logo') ? Helpers::update('shop/', $curr_logo->shop_logo, 'png', $request->file('shop_logo')) : $curr_logo->shop_logo,
+            'time_zone' => $request['time_zone'],
+            'vat_reg_no' => $request['vat_reg_no'],
         ]);
 
-        DB::table('business_settings')->updateOrInsert(['key' => 'shop_email'], [
-            'value' => $request['shop_email']
+        DB::table('mpesa_credentials')->updateOrInsert(['company_id' => $company_id], [
+            'consumer_key' => $request['consumer_key'],
+            'consumer_secret' => $request['consumer_secret'],
+            'test_consumer_key' => $request['test_consumer_key'],
+            'test_consumer_secret' => $request['test_consumer_secret'],
+            'environment' => $request['environment'],
+            'shortcode' => $request['shortcode'],
+            'security_credential' => $request['security_credential'],
+            'lipa_na_mpesa_passkey' => $request['lipa_na_mpesa_passkey'],
         ]);
 
-        DB::table('business_settings')->updateOrInsert(['key' => 'shop_phone'], [
-            'value' => $request['shop_phone']
-        ]);
-
-        DB::table('business_settings')->updateOrInsert(['key' => 'shop_address'], [
-            'value' => $request['shop_address']
-        ]);
-
-        DB::table('business_settings')->updateOrInsert(['key' => 'pagination_limit'], [
-            'value' => $request['pagination_limit']
-        ]);
-
-        DB::table('business_settings')->updateOrInsert(['key' => 'stock_limit'], [
-            'value' => $request['stock_limit']
-        ]);
-
-        DB::table('business_settings')->updateOrInsert(['key' => 'currency'], [
-            'value' => $request['currency']
-        ]);
-
-        DB::table('business_settings')->updateOrInsert(['key' => 'country'], [
-            'value' => $request['country']
-        ]);
-
-        DB::table('business_settings')->updateOrInsert(['key' => 'footer_text'], [
-            'value' => $request['footer_text']
-        ]);
-
-        $curr_logo = BusinessSetting::where(['key' => 'shop_logo'])->first();
-        DB::table('business_settings')->updateOrInsert(['key' => 'shop_logo'], [
-            'value' => $request->has('shop_logo') ? Helpers::update('shop/', $curr_logo->value, 'png', $request->file('shop_logo')) : $curr_logo->value
-        ]);
-
-        DB::table('business_settings')->updateOrInsert(['key' => 'time_zone'], [
-            'value' => $request['time_zone'],
-        ]);
-        DB::table('business_settings')->updateOrInsert(['key' => 'vat_reg_no'], [
-            'value' => $request['vat_reg_no'],
-        ]);
         Toastr::success(translate('Settings updated'));
         return back();
     }
