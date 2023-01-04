2b0c5c5e08321838ae632d6b958fa5453ba42e38
Evans Wanguba <ewanguba@gmail.com>
2023-01-04 11:06:45 +0300

    Roles, permissions and users

--- resources/views/admin-views/roles/index.blade.php
+++ resources/views/admin-views/roles/index.blade.php
@@ -36,7 +36,7 @@
                         <hr>
                         <div class="row pl-2">
                             @foreach($permissions as $permission)
-                                <div class="col-12 col-sm-3">
+                                <div class="col-12 col-sm-4">
                                     <div class="form-group">
                                         <input type="checkbox" id="{{ $permission->id }}" name="permissions[]" value="{{ $permission->id }}">
                                         <label for="{{ $permission->id }}">{{ $permission->name }}</label><br>
